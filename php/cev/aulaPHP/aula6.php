834fd148ad512fffafa9d7cbca6da507d5956679
Igor Farias <balestfarias@gmail.com>
2022-09-12 11:37:20 -0300

    3º semestre
    go!

---
+++ php/cev/aulaPHP/aula6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $c = 2;
+
+        $a = $_GET["a"];
+        $b = $_GET["b"];
+
+        $c += $b; 
+        
+        
+
+    ?>
+</div>
+</body>
+</html>
