3e97043c28358bfdcdd79b4bf03d15077eeeab15
Igor Farias <balestfarias@gmail.com>
2023-01-25 16:52:07 -0300

    C# -00

---
+++ C#/Estudonauta/Ex000/WindowsEx000/WindowsEx000/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsEx000
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e){
+            LblMsg.Text = "Olá, Mundo!";
+        }
+    }
+}
